6302c963a26e0615a90e742179d09bc33215a4ed
Uğur Oruç <uoruc5@hotmail.com>
2018-12-30 19:26:33 +0300

    Updates

---
+++ precache-manifest.1b135f098283dc0ed15272fd3f20e8ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31c826ea60eca8d0f125",
+    "url": "/Cards-vs-Humans/static/js/runtime~main.31c826ea.js"
+  },
+  {
+    "revision": "d3d2ca9888d165bf87e6",
+    "url": "/Cards-vs-Humans/static/js/main.d3d2ca98.chunk.js"
+  },
+  {
+    "revision": "4c4c8df7adebaa612bf4",
+    "url": "/Cards-vs-Humans/static/js/1.4c4c8df7.chunk.js"
+  },
+  {
+    "revision": "d3d2ca9888d165bf87e6",
+    "url": "/Cards-vs-Humans/static/css/main.de0685c4.chunk.css"
+  },
+  {
+    "revision": "0af9c0eef2d450712c9c347357f06a8a",
+    "url": "/Cards-vs-Humans/index.html"
+  }
+];
